125dd64a6582d5024c7e2b1c064caeafa7024d77
Challa Parnika <challaparnika09@gmail.com>
2026-05-02 02:20:08 +0530

    updated home

--- src/main/java/com/dietary/controller/HomeController.java
+++ src/main/java/com/dietary/controller/HomeController.java
@@ -8,6 +8,12 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return " Backend is running successfully!";
+        return "<h1>Dietary Analysis Backend 🚀</h1>"
+             + "<p>Project is deployed successfully.</p>"
+             + "<p><b>Available APIs:</b></p>"
+             + "<ul>"
+             + "<li>POST /api/auth/register</li>"
+             + "<li>POST /api/auth/login</li>"
+             + "</ul>";
     }
 }
